fc8672e50dd428fcedc12fae26a63a69b75971eb
Jodu555 <Jodu505@gmail.com>
2021-09-14 12:47:05 +0200

    Implemented Auth

--- src/utils/sockets/client.js
+++ src/utils/sockets/client.js
@@ -1,10 +1,11 @@
 const { Database } = require('@jodu555/mysqlapi');
 const database = Database.getDatabase();
+const authManager = require('./utils/authManager');
 
 const clients = new Map();
 
-function setupForClient(socket) {
 
+function setupForClient(socket) {
     socket.on('disconnect', () => {
         console.log('Client disconnected');
         clients.delete(socket.id);
@@ -15,6 +16,16 @@ function setupForClient(socket) {
         clients.get(socket.id).serverUUID = data.serverUUID;
     });
 
+    socket.on('auth', (data) => {
+        const user = authManager.getUser(data.token);
+        if (user) {
+            clients.get(socket.id).authenticated = true;
+            clients.get(socket.id).auth_token = data.token;
+            clients.get(socket.id).user = user;
+        } else {
+            socket.emit('auth', false);
+        }
+    });
 
     if (!clients.has(socket.id)) {
         clients.set(socket.id, {
@@ -22,8 +33,12 @@ function setupForClient(socket) {
             socketID: socket.id,
             socketIP: socket.handshake.address,
             serverUUID: '',
+            authenticated: false,
+            auth_token: '',
+            user: null,
         });
-        socket.emit('auth', true);
+    } else {
+        socket.emit('auth', false);
     }
 }
 
